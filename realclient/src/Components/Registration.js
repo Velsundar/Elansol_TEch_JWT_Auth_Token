d067c0fb66a9941ce2fe9c8cf36572adbfa8f829
Sundaravel <sundaravel.exe@gmail.com>
2023-07-17 10:42:34 +0530

    Changed DOM Structure in registration

--- realclient/src/Components/Registration.js
+++ realclient/src/Components/Registration.js
@@ -33,34 +33,34 @@ const Registration = () => {
     };
     return (
         <div id='Registration'>
-            <section class="bg-white">
-                <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+            <section className="bg-white">
+                <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
                     <section
-                        class="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6"
+                        className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6"
                     >
                         <img
                             alt="Night"
                             src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                            class="absolute inset-0 h-full w-full object-cover opacity-80"
+                            className="absolute inset-0 h-full w-full object-cover opacity-80"
                         />
 
-                        <div class="hidden lg:relative lg:block lg:p-12">
+                        <div className="hidden lg:relative lg:block lg:p-12">
                         </div>
                     </section>
 
                     <main
-                        class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+                        className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
                     >
-                        <div class="max-w-xl lg:max-w-3xl">
-                            <div class="relative -mt-16 block lg:hidden">
+                        <div className="max-w-xl lg:max-w-3xl">
+                            <div className="relative -mt-16 block lg:hidden">
 
                             </div>
 
-                            <form action="#" class="mt-8 grid grid-cols-6 gap-6" onSubmit={handleSubmit}>
-                                <div class="col-span-6 sm:col-span-3">
+                            <form action="#" className="mt-8 grid grid-cols-6 gap-6" onSubmit={handleSubmit}>
+                                <div className="col-span-6 sm:col-span-3">
                                     <label
-                                        for="FirstName"
-                                        class="block text-sm font-medium text-gray-700"
+                                        htmlFor="FirstName"
+                                        className="block text-sm font-medium text-gray-700"
                                     >
                                         First Name
                                         { }
@@ -70,7 +70,7 @@ const Registration = () => {
                                         type="text"
                                         id="FirstName"
                                         name="first_name"
-                                        class="mt-1 border p-2 w-full rounded-md border-red-800 bg-white text-md text-black shadow-sm"
+                                        className="mt-1 border p-2 w-full rounded-md border-red-800 bg-white text-md text-black shadow-sm"
                                         value={name}
                                         onChange={(e) => setName(e.target.value)}
                                     />
@@ -90,8 +90,8 @@ const Registration = () => {
                                     />
                                 </div>
 
-                                <div class="col-span-6">
-                                    <label for="Email" class="block text-sm font-medium text-gray-700">
+                                <div className="col-span-6">
+                                    <label htmlFor="Email" className="block text-sm font-medium text-gray-700">
                                         Email
                                     </label>
 
@@ -99,16 +99,16 @@ const Registration = () => {
                                         type="email"
                                         id="Email"
                                         name="email"
-                                        class="mt-1 border p-2  w-full rounded-md border-red-800 bg-white text-sm text-black shadow-sm"
+                                        className="mt-1 border p-2  w-full rounded-md border-red-800 bg-white text-sm text-black shadow-sm"
                                         value={email}
                                         onChange={(e) => setEmail(e.target.value)}
                                     />
                                 </div>
 
-                                <div class="col-span-6 sm:col-span-3">
+                                <div className="col-span-6 sm:col-span-3">
                                     <label
-                                        for="Password"
-                                        class="block text-sm font-medium text-gray-700"
+                                        htmlFor="Password"
+                                        className="block text-sm font-medium text-gray-700"
                                     >
                                         Password
                                     </label>
@@ -117,7 +117,7 @@ const Registration = () => {
                                         type="password"
                                         id="Password"
                                         name="password"
-                                        class="mt-1 p-2 border w-full rounded-md border-red-800 bg-white text-sm text-black shadow-sm"
+                                        className="mt-1 p-2 border w-full rounded-md border-red-800 bg-white text-sm text-black shadow-sm"
                                         value={password}
                                         onChange={(e) => setPassword(e.target.value)}
                                     />
@@ -125,43 +125,43 @@ const Registration = () => {
 
 
 
-                                <div class="col-span-6">
-                                    <label for="MarketingAccept" class="flex gap-4">
+                                <div className="col-span-6">
+                                    <label htmlFor="MarketingAccept" className="flex gap-4">
                                         <input
                                             type="checkbox"
                                             id="MarketingAccept"
                                             name="marketing_accept"
-                                            class="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm"
+                                            className="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm"
                                         />
 
-                                        <span class="text-sm text-gray-700">
+                                        <span className="text-sm text-gray-700">
                                             I want to receive emails about events, product updates and
                                             company announcements.
                                         </span>
                                     </label>
                                 </div>
 
-                                <div class="col-span-6">
-                                    <p class="text-sm text-gray-500">
+                                <div className="col-span-6">
+                                    <p className="text-sm text-gray-500">
                                         By creating an account, you agree to our
-                                        <a href="#" class="text-gray-700 underline">
+                                        <a href="#terms" className="text-gray-700 underline">
                                             terms and conditions
                                         </a>
                                         and
-                                        <a href="#" class="text-gray-700 underline">privacy policy</a>.
+                                        <a href="#policy" className="text-gray-700 underline">privacy policy</a>.
                                     </p>
                                 </div>
 
-                                <div class="col-span-6 sm:flex sm:items-center sm:gap-4">
+                                <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
                                     <button
-                                        class="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
+                                        className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
                                     >
                                         Create an account
                                     </button>
 
-                                    <p class="mt-4 text-sm text-gray-500 sm:mt-0">
+                                    <p className="mt-4 text-sm text-gray-500 sm:mt-0">
                                         Already have an account?
-                                        <a href="/" class="text-gray-700 underline">Log in</a>.
+                                        <a href="/" className="text-gray-700 underline">Log in</a>.
                                     </p>
                                 </div>
                             </form>
